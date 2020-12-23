3e74dba8fd14c624e70f2608bd54dd4be5292390
Gusabary <gusabary@126.com>
2020-12-23 22:13:19 +0800

    chore: integrate with spdlog

--- src/uno_server.cc
+++ src/uno_server.cc
@@ -1,8 +1,11 @@
+#include <spdlog/spdlog.h>
+
 #include "game/server_side/game_manager_s.h"
 
 using SailGame::Game::GameManager;
 
 int main(int argc, char** argv) {
+    spdlog::info("Hello, I'm Uno Server!");
     GameManager gameManager;
     gameManager.Start();
 
