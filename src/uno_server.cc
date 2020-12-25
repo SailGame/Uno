c17719d8c00ee5cd295c0d2a85eba18d2d3feaae
Gusabary <gusabary@126.com>
2020-12-25 14:10:01 +0800

    feat: add StateMachine skeleton code

--- src/uno_server.cc
+++ src/uno_server.cc
@@ -3,6 +3,8 @@
 #include "common/game_manager.h"
 #include "game/state.h"
 
+using Core::ProviderMsg;
+using Core::RegisterArgs;
 using SailGame::Common::GameManager;
 using SailGame::Game::State;
 
@@ -10,6 +12,16 @@ int main(int argc, char** argv) {
     spdlog::info("Hello, I'm Uno Server!");
     GameManager<State> gameManager;
     gameManager.Start();
+
+    ProviderMsg msg;
+    auto registerArgs = msg.mutable_registerargs();
+    registerArgs->set_id("uno");
+    registerArgs->set_gamename("UNO");
+    auto gameSettings = registerArgs->mutable_gamesetting();
+    gameSettings->set_minusers(2);
+    gameSettings->set_minusers(4);
+
+    gameManager.ProcessEvent(std::make_shared<ProviderMsg>(msg));
 
     return 0;
 }
